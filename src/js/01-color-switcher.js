d4ca85db8641c134d4a9395f7fdc5a2bc580d5df
giligran <kuprykovrodion@gmail.com>
2023-05-30 17:14:42 +0300

    sad

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const body = document.querySelector('body')
 
+ function getRandomHexColor() {
+  return body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+ }
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+const switcher = {
+    start() {
+       timerSwitchColor = setInterval(getRandomHexColor, 1000);
+
+    }
+}
+
+btnStart.addEventListener('click', () => {
+}
+)
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerSwitchColor);
+})
