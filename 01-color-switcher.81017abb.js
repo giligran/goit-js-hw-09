fee884e1d059f34398a6f70b302a62ffe82fe1e1
giligran <118477139+giligran@users.noreply.github.com>
2023-05-30 15:11:29 +0000

    Deploying to gh-pages from @ giligran/goit-js-hw-09@df63bcb8c1f451c3d2d16d898fa0d5fb72f80e3c 🚀

--- 01-color-switcher.81017abb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body");const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{})),e.addEventListener("click",(()=>{clearInterval(timerSwitchColor)}));
-//# sourceMappingURL=01-color-switcher.81017abb.js.map
--- 01-color-switcher.abaa4298.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("body");var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){})),e.addEventListener("click",(function(){clearInterval(timerSwitchColor)}))}();
-//# sourceMappingURL=01-color-switcher.abaa4298.js.map
