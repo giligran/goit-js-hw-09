df63bcb8c1f451c3d2d16d898fa0d5fb72f80e3c
giligran <kuprykovrodion@gmail.com>
2023-05-30 18:11:06 +0300

    task-01 color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,28 @@
 const body = document.querySelector('body')
-
- function getRandomHexColor() {
-  return body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
- }
-
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
-
 const switcher = {
-    start() {
-       timerSwitchColor = setInterval(getRandomHexColor, 1000);
-
+    intervalId: null,
+    getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+      },
+    start () {
+        btnStart.disabled = true;
+        btnStop.disabled = false;
+        this.intervalId = setInterval(()=>{
+            body.style.backgroundColor = this.getRandomHexColor();
+        },1000)
+    },
+    stop () {
+        clearInterval(this.intervalId)
+        btnStart.disabled = false;
+        btnStop.disabled = true;
     }
 }
 
-btnStart.addEventListener('click', () => {
-}
-)
-
-btnStop.addEventListener('click', () => {
-    clearInterval(timerSwitchColor);
+btnStart.addEventListener('click', ()=>{
+    switcher.start();
+})
+btnStop.addEventListener('click', ()=> {
+    switcher.stop();
 })
